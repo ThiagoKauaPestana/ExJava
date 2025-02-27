f8dc2a0ff3eab7f1ea421025804419b78e267f09
Thiago Kauã Pestana <145944462+ThiagoKauaPestana@users.noreply.github.com>
2025-02-27 01:25:43 +0000

    TESTE

---
+++ Ex1/src/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Quantas publicacoes voce quer analisar: ");
+        
+        int totalcurtidas = 0;
+        int num_pub = entrada.nextInt();
+        
+        for(int i=1;;
+
+            System.out.print("Quantas curitidas a publicação #%d recebeu: ");
+            int num_curtidas = entrada.nextInt();
+            totalcurtidas = totalcurtidas + num_curtidas;
+
+            if (num_curtidas >= 100){
+                System.out.print("Publicação Com sucesso");
+              
+            }
+            else{
+                System.out.print("Publicação com poucas curtidas");
+              
+            }
+
+        }
+        entrada.close();
+
+    }
+}
