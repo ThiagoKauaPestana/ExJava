55d2fdd3f358f349131b957ab5ff279305b1f84f
Thiago Kauã Pestana <145944462+ThiagoKauaPestana@users.noreply.github.com>
2025-04-07 20:48:43 +0000

    Exercicio Conta Bancaria

--- EX3/ContaBancaria/src/ContaBancaria.java
+++ EX3/ContaBancaria/src/ContaBancaria.java
@@ -1,32 +1,34 @@
 public class ContaBancaria {
-   double numeroConta;
-   String titular;
-   double saldo;
+    String numeroConta;
+    String titular;
+    double saldo;
 
-   public ContaBancaria(double numeroConta, String titular, double saldo){
-        this.titular = titular;
+    public ContaBancaria(String numeroConta, String titular, double saldoInicial) {
         this.numeroConta = numeroConta;
-        this.saldo = saldo;
-
-   }
-
-   public double depositar(double valor){
-    if (valor > 0 && saldo >= valor) {
-        return saldo -= valor;
-        
-
-    } else{
-        System.out.println("Nao foi possivel, Saldo insuficiente");
+        this.titular = titular;
+        this.saldo = saldoInicial;
     }
+
     
-   }
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
 
-   public double sacar(double valor){
-        return saldo - valor;
+    
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
 
-   }
-
-   public double exibirSaldo(){
-    return saldo;
-   }
+    }
+        public void exibirSaldo() {
+            System.out.printf("Saldo atual: R$ %.2f\n", saldo);
+        }
 }
+
--- EX3/ContaBancaria/src/TestaContaBancaria.java
+++ EX3/ContaBancaria/src/TestaContaBancaria.java
@@ -1,51 +1,11 @@
-import java.util.Scanner;
-
 public class TestaContaBancaria {
     public static void main(String[] args) {
-        ContaBancaria Conta = new ContaBancaria(10420686,  "Thiago", 1000);
-        Scanner entrada = new Scanner(System.in);
+        ContaBancaria conta = new ContaBancaria("10420686", "Thiago", 1000);
 
-        int op;
-        double valor;
-
-        do{
-
-            System.out.println("1- Depositar");
-            System.out.println("2- Sacar");
-            System.out.println("3- Exibir Saldo");
-            System.out.println("4- Sair");
-            System.out.print("\nDigite a Opcao que quer realizar: ");
-            op = entrada.nextInt();
-
-            switch (op) {
-                case 1:
-
-                    System.out.println("Digite o valor: ");
-                    valor = entrada.nextDouble();
-                    Conta.depositar(valor);
-                    System.out.printf("Seu Saldo: %d\n", Conta.saldo);
-                    break;
-
-                case 2:
-                    System.out.println("Digite o valor: ");
-                    valor = entrada.nextDouble();
-                    Conta.sacar(valor);
-                    System.out.printf("Seu Saldo: %d\n", Conta.saldo);
-                    break;
-                case 3:
-                    Conta.exibirSaldo();
-                    System.out.printf("Seu saldo: %.2f\n", Conta.saldo);
-                    break;
-                case 4:
-                    break;
-                default:
-                    System.out.println("Opcao Inexistente");
-            }
-        } while(op != 4);
-
-        entrada.close();
+        conta.depositar(600.00);
+        conta.sacar(200.00);
+        conta.exibirSaldo();
+        
+        System.out.printf("%s  %s  %.2f\n", conta.numeroConta, conta.titular, conta.saldo);
     }
-
-    
-    
 }
